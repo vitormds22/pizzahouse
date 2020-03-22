f134833523cc85df357e5075ccd4ae9207e6aef4
Vitor Machado <vitormds22@gmail.com>
2020-03-22 18:38:09 -0300

    Change style and query params

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,20 +1,19 @@
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div class="title m-b-md">
-                Pizzas List
-            </div>                
-            {{-- @for($i = 0; $i < 5; $i++)
-                <p>the value of i is {{ $i }}</p>
-            @endfor --}}
-            
-            {{-- @for($i = 0; $i < count($pizzas); $i++)
-                <p>{{ $pizzas[$i]['type'] }}</p>
-            @endfor --}}
-                
-            @foreach ($pizzas as $pizza)
-                <div>
-                    {{ $pizza['type'] }} - {{ $pizza['base'] }}
-                </div>
-            @endforeach
-        </div>
+@extends('layouts.layout')    
+
+@section('content')
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Pizzas List
+        </div>                
+
+        <p>{{ $name }} - {{ $age }} years</p>
+
+        @foreach ($pizzas as $pizza)
+            <div>
+                {{ $pizza['type'] }} - {{ $pizza['base'] }}
+            </div>
+        @endforeach
     </div>
+</div>
+@endsection
