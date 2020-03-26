d3291deee06402d124f951ac37a2fa2e7693878e
Vitor Machado <vitormds22@gmail.com>
2020-03-26 11:27:28 -0300

    Add datas in db

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -28,4 +28,17 @@ class PizzaController extends Controller
         return view('pizzas.create');
     }
 
+    public function store(){
+        
+        $pizza = new Pizza();
+
+        $pizza->name = request('name');
+        $pizza->type = request('type');
+        $pizza->base = request('base');
+        $pizza->price = request('price');
+
+        $pizza->save();
+
+        return redirect('/')->with('msg', 'Thanks for your order');
+    }
 }
--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -4,8 +4,12 @@
 <div class="wrapper create-pizza">
     <h1>Create a new Pizza</h1>
     <form action="/pizzas" method="POST">
+        @csrf
         <label for="name">Your name: </label>
         <input type="text" id="name" name="name">
+
+        <label for="name">The price: </label>
+        <input type="text" id="price" name="price">
         
         <label for="type">Choose pizza type:</label>
         <select name="type" id="type">
@@ -16,7 +20,7 @@
         </select>
 
         <label for="base">Choose pizza type:</label>
-        <select name="type" id="type">
+        <select name="base" id="base">
             <option value="Cheesy Crust">Cheesy Crust</option>
             <option value="Garlic Crust">Garlic Crust</option>
             <option value="Thin & Crispy">Thin & Crispy</option>
